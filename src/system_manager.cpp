77389e2be61c2a66d1bf7d44693fbc384ab1e5e2
gis1501 <gis1501@gmail.com>
2020-04-03 21:28:40 +0300

    edit

--- src/system_manager.cpp
+++ src/system_manager.cpp
@@ -1,22 +1,106 @@
 #include "system_manager.h"
 
 #include <QDebug>
+#include <QDir>
+#include <QTimer>
+#include <QCoreApplication>
+#include <QPluginLoader>
+
+
+#ifdef Q_OS_WIN
+static const QString lib_prefix = "";
+static const QString lib_extension = "dll";
+#elif defined Q_OS_MACX
+static const QString lib_extension = "dylib";
+static const QString lib_prefix = "lib";
+#else
+static const QString lib_extension = "so";
+static const QString lib_prefix = "lib";
+#endif
 
 system_manager::system_manager(QObject *parent) : QObject(parent)
 {
-
+    known_plugins.insert("ui_controller", "1.0.0");
 }
 
 bool system_manager::initialization()
 {
-    qDebug() << tr("Start initialization ...");
+    qDebug() << Q_FUNC_INFO << tr("Start initialization ...");
+
+    load_plugins();
 
     return false;
 }
 
 void system_manager::program_launch(bool is_init_state)
 {
-    qDebug() << tr("Program launch ...");
+    qDebug() << Q_FUNC_INFO << tr("Program launch ...");
 
     Q_UNUSED(is_init_state)
+}
+
+bool system_manager::is_known_plugins(const QString &name, const QString &version)
+{
+    bool is_name(false);
+    bool is_version(false);
+
+    is_name = known_plugins.contains(name);
+
+    if(is_name)
+        if(known_plugins.value(name) == version)
+            is_version = true;
+
+    if(is_name && is_version)
+        return true;
+    else
+        return false;
+}
+
+void system_manager::load_plugins()
+{
+    qDebug() << Q_FUNC_INFO << tr("Load all Plugins ...");
+
+    QTimer plugin_load_time;
+    plugin_load_time.start();
+
+    QDir plugins_dir = QDir(qApp->applicationDirPath());
+    plugins_dir.cd("plugins");
+
+    qDebug() << Q_FUNC_INFO << "Loading plugins from:" << plugins_dir.path();
+
+    QStringList plugin_filter;
+    plugin_filter << QString("*.%1").arg(lib_extension);
+    QStringList plugins_list = plugins_dir.entryList(plugin_filter);
+
+    foreach (QString candidate, plugins_list)
+    {
+        QString file_name = QString("%1").arg(plugins_dir.absoluteFilePath(candidate));
+        qDebug() << Q_FUNC_INFO << "Loading" << file_name;
+
+        QPluginLoader plugin_loader(file_name);
+        QJsonObject meta_data = plugin_loader.metaData();
+        qDebug() << Q_FUNC_INFO << "MetaData:" << meta_data;
+
+        QString plugin_name = meta_data.value("MetaData").toObject().value("name").toString();
+        QString plugin_version = meta_data.value("MetaData").toObject().value("version").toString();
+
+        qDebug() << Q_FUNC_INFO << "Plugin name:" << plugin_name;
+        qDebug() << Q_FUNC_INFO << "Plugin version:" << plugin_version;
+
+        // Hardcoded
+        if(is_known_plugins(plugin_name, plugin_version))
+        {
+            if(plugin_name == "ui_controller")
+                load_ui_controller_plugin(plugin_loader, plugin_name, plugin_version);
+        }
+    }
+
+    qDebug() << Q_FUNC_INFO << tr("Finished loading:") << plugin_load_time.interval() << tr("ms");
+}
+
+void system_manager::load_ui_controller_plugin(QPluginLoader &plugin_loader, const QString plugin_name, const QString plugin_version_number)
+{
+    qDebug() << Q_FUNC_INFO;
+
+    bool is_load_plugin(false);
 }
--- src/system_manager.h
+++ src/system_manager.h
@@ -2,6 +2,9 @@
 #define SYSTEM_MANAGER_H
 
 #include <QObject>
+#include <QMap>
+
+class QPluginLoader;
 
 class system_manager : public QObject
 {
@@ -15,7 +18,13 @@ public:
 signals:
 
 private:
+    // Hardcoded list of known plugins.
+    QMap<QString, QString> known_plugins;
+    bool is_known_plugins(const QString &name, const QString &version);
 
+    // Load all Plugins
+    void load_plugins();
+    void load_ui_controller_plugin(QPluginLoader &plugin_loader, const QString plugin_name, const QString plugin_version_number);
 };
 
 #endif // SYSTEM_MANAGER_H
